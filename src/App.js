b6146ab4e0f65533b3c09b959202de755dd9b2ee
sulyvan <sulyvan.weppe@gmail.com>
2023-01-19 16:08:30 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import RoundDetailsHeader from './Components/GameIn/RoundDetailsHeader';
+import StartNewGameHeader from './Components/Start/StartNewGameHeader';
+import StartNewGamePlayersEntry from './Components/Start/StartNewGamePlayersEntry';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <RoundDetailsHeader />
+            </div>
+        );
+    }
 }
 
-export default App;
+export default App
