50f965f8cb0454824b34b6ceab5c30f7f2b823cc
sulyvan <sulyvan.weppe@gmail.com>
2023-02-27 15:27:50 +0100

    add drawer enabling to navigate between new game and all games display

---
+++ src/Components/AllGames/AllGames.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class AllGames extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default AllGames;
